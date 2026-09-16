d5076be063ac581373221beac3ec08741a88f90a
jospeale07 <jospeale07@gmail.com>
2026-09-16 17:15:49 -0400

    update Enunciado_1/Main.py

--- Enunciado_1/Main.py
+++ Enunciado_1/Main.py
@@ -3,11 +3,7 @@ from Perro import Perro
 from Gato import Gato
 from Vaca import Vaca 
 
-animales = [
-  Perro("Inu", 3)
-  Gato("Neko", 2)
-  Vaca("Ushi", 5)
-]
+animales = [Perro("Inu", 3), Gato("Neko", 2), Vaca("Ushi", 5)]
 
 for animal in animales:
   animal.hacer_sonido()
