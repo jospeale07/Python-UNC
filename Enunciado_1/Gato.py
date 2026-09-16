358b0adda7bb9f23da27e862e7a30892fe1fb2cb
jospeale07 <jospeale07@gmail.com>
2026-09-16 16:50:55 -0400

    update Enunciado_1/Gato.py

--- Enunciado_1/Gato.py
+++ Enunciado_1/Gato.py
@@ -2,5 +2,6 @@ from Animal import Animal
 from Perro import Perro
 
 class Gato(Animal):  
-  def Animal(self, Gato, Miau):
+  def hacer_sonido(self):
+    print(f"{self.nombre} dice: ¡Miau Miau!")
       
