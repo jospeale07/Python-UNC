be1b2d29778d691962d1a0a933235137e5fd7c15
jospeale07 <jospeale07@gmail.com>
2026-09-16 16:51:11 -0400

    update Enunciado_1/Vaca.py

--- Enunciado_1/Vaca.py
+++ Enunciado_1/Vaca.py
@@ -2,6 +2,7 @@ from Animal import Animal
 from Perro import Perro
 from Gato import Gato
 
-class Gato(Animal):  
-  def Animal(self, Perro, Guau):
+class Vaca(Animal):  
+  def hacer_sonido(self):
+    print(f"{self.nombre} dice: ¡Muuuu!")
       
